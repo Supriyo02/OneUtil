ea89724a61cc44ca0f2fbe19e1fc3407e1c9692d
Supriyo <supriyoghosh27125@gmail.com>
2024-08-20 22:30:40 +0530

    added my-booking page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ import EditService from "./pages/EditService";
 import Search from "./pages/Search";
 import Detail from "./pages/Detail";
 import Booking from "./pages/Booking";
+import MyBookings from "./pages/MyBookings";
 
 const App = () => {
   const { isLoggedIn } = useAppContext();
@@ -99,6 +100,15 @@ const App = () => {
               element={
                 <Layout>
                   <MyServices />
+                </Layout>
+              }
+            />
+
+            <Route
+              path="/my-bookings"
+              element={
+                <Layout>
+                  <MyBookings />
                 </Layout>
               }
             />
--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -202,3 +202,15 @@ export const createServiceBooking = async(formData: BookingFormData) =>{
         throw new Error("Error booking service");
     }
 };
+
+export const fetchMyBookings = async(): Promise<ServiceType[]> =>{
+    const response = await fetch(`${API_BASE_URL}/api/my-bookings`, {
+        credentials: "include",  
+    });
+
+    if(!response.ok){
+        throw new Error("Unable to fetch bookings")
+    }
+
+    return response.json();
+};
